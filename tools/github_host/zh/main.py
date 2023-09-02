44b44839dbc94ce51d64b87ec842287ff054a97f
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2023-09-02 15:28:42 +0800

    Update main.py

--- tools/github_host/zh/main.py
+++ tools/github_host/zh/main.py
@@ -8,7 +8,7 @@ import time
  
 with open("./hosts.txt","w") as f:
     f.write("-------------\n请将下面的数据复制,关闭并粘贴到下一个打开的记事本中(替代上一次复制的)\n-------------\n\n")
-    f.write(requests.get("https://gitee.com/fliu2476/github-hosts/raw/main/hosts").text)
+    f.write(requests.get("https://gitee.com/frankwuzp/github-host/raw/main/hosts").text)
 os.system("notepad {path}".format(path=os.getcwd()+"/hosts.txt"))
 subprocess.call('cmd /c shell.vbs')
 input("复制完成了吗?请在复制完成后回到本页面按下回车(hosts文件记得Ctrl+S保存再回车)")
