829bd197a005f550d8b5415ab5bf9fa25ce643d5
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2024-08-27 21:44:42 +0800

    Smart-KTV(Developing) 0001

--- tools/Smart_KTV/main.py
+++ tools/Smart_KTV/main.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 import easygui
 import ffmpeg
-import matplotlib.pyplot as plt
 import numpy as np
 import wave
-from scipy.signal import savgol_filter,find_peaks
+# import spleeter
+# import spleeter.separator
 
 def get_wave(audio_path):
     # 打开WAV文档
@@ -20,27 +20,11 @@ def get_wave(audio_path):
     wave_data = np.fromstring(str_data, dtype=np.short)
     wave_data.shape = -1, 2
     wave_data = wave_data.T
-    wave_data = np.absolute(wave_data)
-    p , _ = find_peaks
-    time = np.arange(0, nframes) * (1.0 / framerate)
-    # 绘制波形
-    plt.subplot(211) 
-    plt.plot(time[:1500:2], wave_data[0][:1500:2])
-    plt.subplot(212) 
-    plt.plot(time[:1500:2], wave_data[1][:1500:2], c="g")
-    plt.xlabel("time (seconds)")
-    plt.show()
-    # savgol_filter(wave_data[0], 5, 3, mode= 'nearest')
-    # 绘制波形
-    plt.subplot(211) 
-    plt.plot(time[:1500:2], savgol_filter(wave_data[0], 5, 3, mode= 'nearest')[:1500:2])
-    plt.subplot(212) 
-    plt.plot(time[:1500:2], savgol_filter(wave_data[1], 5, 3, mode= 'nearest')[:1500:2], c="g")
-    plt.xlabel("time (seconds)")
-    plt.show()
+    return wave_data
 if __name__ == '__main__':
     audio_path = '1.wav'
     
-    # ffmpeg.input(easygui.fileopenbox()).output(audio_path).run() 
+    ffmpeg.input(easygui.fileopenbox()).output(audio_path).run() 
     
-    get_wave(audio_path=audio_path)
+    # sep = spleeter.separator.Separator('spleeter:2stems')
+    # sep.separate_to_file(audio_path,"./")
