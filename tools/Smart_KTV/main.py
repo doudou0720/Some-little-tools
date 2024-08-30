c8373ddd218467db5ad3f363de68cf6c5e927505
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2024-08-30 15:10:13 +0800

    Smart-KTV(Almost Finish!) 0004

--- tools/Smart_KTV/main.py
+++ tools/Smart_KTV/main.py
@@ -5,8 +5,9 @@ app = flask.Flask(__name__, template_folder="./templates", static_folder="./stat
 @app.route("/play")
 def play():
     name = flask.request.args.get("name")
-    if name == None:
-        return flask.Response("Cannot Get key 'name'", 400)
-    return flask.render_template("main.html",name=name)
+    disc = flask.request.args.get("disc")
+    song_name = flask.request.args.get("song_name")
+    singer = flask.request.args.get("singer")
+    return flask.render_template("main.html",name=name,disc=disc,song_name=song_name,singer=singer)
     
 app.run(debug=True,port=8000,host="0.0.0.0")
